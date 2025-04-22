abd6e531e91d70b94ffae074ee9cacd03dd798b1
ArtemAntoshchenko <fgd19611@yndex.ru>
2025-04-22 17:37:17 +0300

    second commit

--- index.js
+++ index.js
@@ -20,3 +20,43 @@ let employees=['john','Doe']
 employees=JSON.stringify(employees)
 let newEmployees=JSON.parse(employees)
 console.log(typeof(newEmployees))
+
+
+
+// 1)
+let obj={
+    "1":'same',
+    '2':'some'
+}
+function countProperties(obj){
+    let newObj=Object.values(obj)
+    return newObj.length
+}
+console.log(countProperties(obj))
+
+// 2)
+let obj1={
+    Tom:5000,
+    Sam:6000,
+    Timmy:4000
+}
+function getMaxSalary(salaries){
+
+}
+
+
+// 3)
+let obj2={
+    "1":50,
+    '2':500
+}
+function averageValues(obj2){
+    let objLength=Object.keys(obj2)
+    let objValues=Object.values(obj2)
+    let sum=0
+    for(let i of objValues){
+        sum+=i
+    }
+    return sum/objLength.length
+}
+console.log(averageValues(obj2))
